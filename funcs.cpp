afdda020b16c32d71cef90c7cc3cb269eb70860b
Vika <schemiakina2011@yandex.ru>
2019-11-14 15:55:57 +0300

    add function MyMul()

--- funcs.cpp
+++ funcs.cpp
@@ -8,4 +8,7 @@ double MySum(double a, double b)
 double MySub(double a, double b)
 {
 	return a - b;
-}
+}double MyMul(double a, double b)
+{
+return a * b;
+}
--- funcs.h
+++ funcs.h
@@ -3,4 +3,6 @@
 #define __FUNCS_H__
 double MySum(double, double);
 double MySub(double, double);
+double MyMul(double, double);
+
 #endif // __FUNCS_H__
